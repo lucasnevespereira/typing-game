7de7e533582c574831b24ffee7401d2420efb66a
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-05-14 18:56:55 +0200

    random text & user input handled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { lorem } from "faker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+
+  handleInput(value: string) {
+    console.log(value);
+  }
 }
